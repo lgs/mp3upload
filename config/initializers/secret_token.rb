1216bde8f40dd2b8f0ad94ae55782f6d519d9975
Luca G. Soave <luca.soave@gmail.com>
2011-02-24 00:18:29 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mp3upload::Application.config.secret_token = 'dd766339ccebfbb5fedbdf8d0b9df0ebf83acf499c28d5c854579a7a17fcef14f722122f8aa88b5a68f7a4a7b18992c71059aeb2bf01d26977649747018f9bee'
