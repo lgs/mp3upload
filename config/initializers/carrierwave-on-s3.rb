0b0ed75fdf09531565ed2970fa9a49cfa06442de
Luca G. Soave <luca.soave@gmail.com>
2011-03-01 07:27:02 +0100

    set fog/s3 to solve ''right_aws library is no longer supported''

--- config/initializers/carrierwave-on-s3.rb
+++ config/initializers/carrierwave-on-s3.rb
@@ -11,8 +11,23 @@
 #       storage :right_s3
 #     end
 
-CarrierWave.configure do |config|
-  config.s3_access_key_id = '1NFZXSPRJV9SEAX1DJ82'
-  config.s3_secret_access_key = 'bZnXN2fK0sFM//a8/Gcn3lnKXXgj5CIPjvyTJuhn'
-  config.s3_bucket = 'soundcloud-mp3upload'
-end
+#first old setting :
+#CarrierWave.configure do |config|
+#  config.s3_access_key_id = '1NFZXSPRJV9SEAX1DJ82'
+#  config.s3_secret_access_key = 'bZnXN2fK0sFM//a8/Gcn3lnKXXgj5CIPjvyTJuhn'
+#  config.s3_bucket = 'soundcloud-mp3upload'
+#end
+
+    CarrierWave.configure do |config|
+      config.fog_credentials = {
+        :provider               => 'AWS',       # required
+        :aws_access_key_id      => '1NFZXSPRJV9SEAX1DJ82',       # required
+        :aws_secret_access_key  => 'bZnXN2fK0sFM//a8/Gcn3lnKXXgj5CIPjvyTJuhn',       # required
+        :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+      }
+      config.fog_directory  = 'soundcloud-mp3upload'                     # required
+      config.fog_host       = nil #'https://assets.example.com'            # optional, defaults to nil
+      config.fog_public     = false                                   # optional, defaults to true
+      config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+    end
+
