e6b7bb3d76adcdd15265505edcd8c22abed2dba0
Luca G. Soave <luca.soave@gmail.com>
2011-03-08 18:12:18 +0100

    fix Rack::Static for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Mp3upload::Application.configure do
 
   # Settings specified here will take precedence over those in config/application.rb
-  config.middleware.use Rack::Static, :urls => ['/uploads'], :root => 'tmp'
+  # config.middleware.use Rack::Static, :urls => ['/uploads'], :root => 'tmp'
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
